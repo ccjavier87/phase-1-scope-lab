24508416e9482322ce9d59544164ba0c720c294c
Carl_Javier <ccjavier87@gmail.com>
2023-02-04 15:21:12 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+
+setBestCustomer = () => {bestCustomer = 'not bob'};
+
+overwriteBestCustomer = () => {bestCustomer = 'maybe bob'};
+
+const leastFavoriteCustomer = 'Assignment to constant variable.';
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'bob';
+}
